e08e58f5fd233887f3fe793dd0a452ed24860e8a
Anuj Fulari <anujfulari@gmail.com>
2025-06-01 01:10:29 +0530

    [ALPHA] Added args: task and wd for main.py

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from agent import Agent
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+import argparse
+import os
 
 load_dotenv("../.env")
 
-llm = ChatOpenAI(model="gpt-4o-mini")
+def invoke_agent(task, working_dir):
+    llm = ChatOpenAI(model="gpt-4o-mini")
 
-ag = Agent(
-    llm,
-    working_directory="/mnt/d/AI/SuperAgentDash",
-)
+    ag = Agent(
+        llm,
+        working_directory=working_dir,
+    )
 
-query = """
-Write comments in the code of SuperAgent written in directory /mnt/d/AI/SuperAgentDash
-"""
+    ag.invoke(task)
 
-ag.invoke(query)
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description="Agent Runner")
+    
+    parser.add_argument("-task", help="Task for agent", required=True)
+    parser.add_argument("-wd", help="Working Directory", default=os.getcwd())
+
+    args = parser.parse_args()
+    print(args)
+    invoke_agent(args.task, args.wd)
